c5803eb70373ef0c0575c9f3b03811382721ee1a
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-07-28 11:39:04 +0530

    javascript type conversion and operators

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,26 @@
-console.log('hiiiii')
+// console.log(10 + "10");
+// console.log(true + false);
+// console.log(null + undefined);
+// console.log(true + false + "javascript");
+// console.log(null + null + undefined);
+
+// console.log(10 - "10");
+// console.log(100 - "java");
+// console.log(true - false);
+
+// console.log(33 + "hi" - Boolean("1") + String(10));
+// console.log(Boolean(1) + Boolean("Hey"));
+// console.log(undefined + String("hii") - 10 * 20);
+
+// console.log(1n- BigInt(10));
+
+// OPERATORS
+
+let a = 10;
+let b = 20;
+let c = 30;
+let sum = a+b+c;
+console.log(sum);
+console.log(a/b);
+
+
