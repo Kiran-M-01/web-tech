d14a5138ecb8f336748fcce6ae28408be9999b32
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-01 10:17:49 +0530

    ternary operators

--- javascript/statements.js
+++ javascript/statements.js
@@ -5,6 +5,7 @@
 // 4)switch
 // 5)ternary operator
 
+// IF
 // let num = 11;
 
 // if (num%2 == 0){
@@ -14,17 +15,25 @@
 //     console.log('odd number')
 // }
 
-let age  = Number(prompt('enter your age'))
-if(age <= 5){
-    console.log('child')
-}
-else if(age < 18){
-    console.log('you cant vote')
-}
-else if(age < 25 ){
-    console.log('can vote')
+// ELSE IF LADDER
+// let age  = Number(prompt('enter your age'))
+// if(age <= 5){
+//     console.log('child')
+// }
+// else if(age < 18){
+//     console.log('you cant vote')
+// }
+// else if(age < 25 ){
+//     console.log('can vote')
 
-}
-else{
-    console.log('aged')
-}
+// }
+// else{
+//     console.log('aged')
+// }
+
+
+// TERNARY OPERATOR
+let a = 3
+let b = 20
+let result = a%2 === 0? 'even':'odd'
+console.log(result)
