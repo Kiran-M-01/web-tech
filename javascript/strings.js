910afd0327d39f8162fcf633bf1e420261b842cb
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-04 11:54:41 +0530

    creating a string

---
+++ javascript/strings.js
@@ -0,0 +1,22 @@
+// STRINGS
+// the collection of multiple charecters is called as strings
+
+// 1) STRING LITERALS
+// 1. single quotes 2. double qoutes 3.backticks(string interpolation/template literals)
+// all three are called string literals
+
+// let a = 'this is a single quotes'
+// console.log(a)
+// let b = "this is a double quotes"
+// console.log(b)
+// let c = `${a} and ${b}`
+// console.log(c)
+
+// 2) STRING OBJECT(string constructor)
+let str = new String("hello")
+console.log(str)
+console.log(typeof str)
+
+// STRING FUNCTION
+let str1 = String("hii")
+console.log('type of str1 = ',typeof str1)
