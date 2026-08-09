007803e399c646ae24cfc6c66394d833e39b6d7b
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-09 10:49:49 +0530

    asynchronous javascript

--- javascript/9star/asynchronous.js
+++ javascript/9star/asynchronous.js
@@ -73,12 +73,42 @@
 //   console.log(v.products)
 // })
 
-let result = fetch('https://dummyjson.com/quotes')
-.then((response) =>{
-    return response.json()
-}).then((data) =>{
-    console.log(data.quotes)
-})
+// let result = fetch('https://dummyjson.com/quotes')
+// .then((response) =>{
+//     return response.json()
+// }).then((data) =>{
+//     // console.log(data.quote)
+//     data.quotes.forEach((value) =>{
+//         // console.log(value)
+//         console.log(`the Author is - ${value.author}`)
+//         console.log(`the Quote is - ${value.quote}`)
+//     })
+// })
 
+// let res = fetch('https://dummyjson.com/carts')
+// .then((response) =>{
+//     return response.json()
+// }).then((v) =>{
+//     // console.log(v.carts)
+//     v.carts.forEach((value) =>{
+//         console.log(value.total)
+//     })
+// })
 
+// ASYNC/AWAIT
+
+async function fetching(){
+    let res = await fetch('https://dummyjson.com/products')
+    console.log(res);
+    let data = await res.json()
+    console.log(data);
+    console.log(data.products);
+    data.products.forEach((v) =>{
+        console.log(v.title)
+        console.log(v.price)
+
+    })
+}
+
+fetching()
 
