417837221ebc5e3388c23571bd179aae75d92288
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-13 11:40:13 +0530

    asynchronous javascript - setInterval()

--- javascript/asynchronuos.js
+++ javascript/asynchronuos.js
@@ -1,7 +1,18 @@
-let timer = setTimeout(() =>{
-    console.log("this is seTimeOut")
-},3000)
+// 1)SETTIMEOUT
+// let timer = setTimeout(() =>{
+//     console.log("this is seTimeOut")
+// },3000)
 
-console.log("hi")
-console.log("hello")
+// console.log("hi")
+// console.log("hello")
+
+//2) SETINTERVAL
+let count = 0
+let timer = setInterval(() =>{
+    count++
+    console.log(count)
+    if(count==5){
+        clearInterval(timer)
+    }
+},4000)
 
