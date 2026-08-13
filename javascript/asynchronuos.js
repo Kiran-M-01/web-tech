b62e80f3032a9c8ed9c9abb8fe5ff3086b575bbd
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-13 12:06:08 +0530

    promise

--- javascript/asynchronuos.js
+++ javascript/asynchronuos.js
@@ -1,3 +1,7 @@
+// ASYNCHRONOUS JAVASCRIPT
+//1} TOMEOUT FUNCTIONS
+
+
 // 1)SETTIMEOUT
 // let timer = setTimeout(() =>{
 //     console.log("this is seTimeOut")
@@ -6,13 +10,26 @@
 // console.log("hi")
 // console.log("hello")
 
-//2) SETINTERVAL
-let count = 0
-let timer = setInterval(() =>{
-    count++
-    console.log(count)
-    if(count==5){
-        clearInterval(timer)
-    }
-},4000)
+// //2) SETINTERVAL
+// let count = 0
+// let timer = setInterval(() =>{
+//     count++
+//     console.log(count)
+//     if(count==5){
+//         clearInterval(timer)
+//     }
+// },4000)
+
+// 3)clearTimeout()
+// 4) clearInterval
+
+// 2} PROMISE - 
+// - sequential completion or failure of an asynchronour task
+
+let a = new Promise((res, rej) =>{
+    rej("it is rejecteed")
+})
+
+console.log(a)
+
 
