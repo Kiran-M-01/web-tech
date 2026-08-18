d533b18e6176dedd6400d9ffeab051263e1689af
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-18 12:04:25 +0530

    dom manipulation methods

--- javascript/DOM/scripts.js
+++ javascript/DOM/scripts.js
@@ -2,5 +2,8 @@ function change() {
     let target = document.getElementById("one")
     console.log(target);
     target.innerHTML = '<h3>this is after change</h3>'
+    target.style.backgroundColor='black'
+    target.style.color = 'white'
+    target.style.padding='20px'
 
 }
