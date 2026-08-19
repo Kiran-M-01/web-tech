4d75990d7fb9d106d95c9abc29c5522ce6822f5e
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-19 11:08:09 +0530

    dom methods

--- javascript/DOM/scripts.js
+++ javascript/DOM/scripts.js
@@ -7,14 +7,28 @@
 //     target.style.padding='20px'
 
 // }
+// function change() {
+//     let target = document.getElementsByClassName("one")
+//     console.log(target);
+//     // target.innerHTML = '<h3>this is after change</h3>'
+//     // target[0].style.backgroundColor='black'
+//     // target[0].style.color = 'white'
+//     for(i=0;i<target.length;i++){
+//         target[i].style.backgroundColor='black'
+//         target[i].style.color = 'white'
+//         target[i].style.padding='20px'
+//     }
+
+// }
+
 function change() {
-    let target = document.getElementsByClassName("one")
+    let target = document.getElementsByTagName("h1")
     console.log(target);
     // target.innerHTML = '<h3>this is after change</h3>'
     // target[0].style.backgroundColor='black'
     // target[0].style.color = 'white'
     for(i=0;i<target.length;i++){
-        target[i].style.backgroundColor='black'
+        target[i].style.backgroundColor='red'
         target[i].style.color = 'white'
         target[i].style.padding='20px'
     }
