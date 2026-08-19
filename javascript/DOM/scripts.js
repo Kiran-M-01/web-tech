44640e7700f2e6214aec6e61ec6cde6eb1c2399b
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-19 11:00:54 +0530

    dom methods

--- javascript/DOM/scripts.js
+++ javascript/DOM/scripts.js
@@ -1,9 +1,22 @@
+// function change() {
+//     let target = document.getElementById("one")
+//     console.log(target);
+//     target.innerHTML = '<h3>this is after change</h3>'
+//     target.style.backgroundColor='black'
+//     target.style.color = 'white'
+//     target.style.padding='20px'
+
+// }
 function change() {
-    let target = document.getElementById("one")
+    let target = document.getElementsByClassName("one")
     console.log(target);
-    target.innerHTML = '<h3>this is after change</h3>'
-    target.style.backgroundColor='black'
-    target.style.color = 'white'
-    target.style.padding='20px'
+    // target.innerHTML = '<h3>this is after change</h3>'
+    // target[0].style.backgroundColor='black'
+    // target[0].style.color = 'white'
+    for(i=0;i<target.length;i++){
+        target[i].style.backgroundColor='black'
+        target[i].style.color = 'white'
+        target[i].style.padding='20px'
+    }
 
 }
