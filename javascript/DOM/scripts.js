3eab24cb10ac8afc2fd4128d42235b98ee41ed01
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-19 11:35:49 +0530

    dom task

--- javascript/DOM/scripts.js
+++ javascript/DOM/scripts.js
@@ -21,17 +21,26 @@
 
 // }
 
-function change() {
-    let target = document.getElementsByTagName("h1")
-    console.log(target);
-    // target.innerHTML = '<h3>this is after change</h3>'
-    // target[0].style.backgroundColor='black'
-    target[0].innerText = 'this is js'
-    target[1].textContent = 'this is changed js'
-    for(i=0;i<target.length;i++){
-        target[i].style.backgroundColor='red'
-        target[i].style.color = 'white'
-        target[i].style.padding='20px'
-    }
+// function change() {
+//     let target = document.getElementsByTagName("h1")
+//     console.log(target);
+//     // target.innerHTML = '<h3>this is after change</h3>'
+//     // target[0].style.backgroundColor='black'
+//     target[0].innerText = 'this is js'
+//     target[1].textContent = 'this is changed js'
+//     for(i=0;i<target.length;i++){
+//         target[i].style.backgroundColor='red'
+//         target[i].style.color = 'white'
+//         target[i].style.padding='20px'
+//     }
 
+// }
+
+function hide(){
+    let target = document.getElementById("box").style.display = "none"
 }
+
+function show(){
+    let target = document.getElementById("box").style.display = "block"
+}
+
