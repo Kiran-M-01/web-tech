1df6feee6ef1b367ea86e9cb44c9bcf73bc106ff
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-08-29 12:11:23 +0530

    react project

--- react/vite-project/src/main.jsx
+++ react/vite-project/src/main.jsx
@@ -1,3 +1,5 @@
+import React from "react"
 import {createRoot} from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(<h1>this is my first react project</h1>)
+let a = React.createElement("div", {id: "one"},React.createElement("h1",null,"this is h1"),React.createElement("h2",null,"this is h2"))
+createRoot(document.getElementById('root')).render(a)
