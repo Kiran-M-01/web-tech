7a1b79f55411df1fa2e53eb8ddf40d5ef4ac2d4b
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-09-02 11:40:27 +0530

    react expressions

--- react/vite-project/src/App.jsx
+++ react/vite-project/src/App.jsx
@@ -1,13 +1,23 @@
 let App = () => {
     let a = 10;
     let b= 20;
+    let c= "this is js"
+    let obj = {
+        name:"react js",
+        age:15
+    }
 
     return (
         <div>
             <h1>{a}</h1>
             <h1>{b}</h1>
+            <h2>{c}</h2>
+            <p>{obj.name}</p>
+            <p>{obj.age}</p>
         </div>
     )
+
+
 }
 
 export default App
