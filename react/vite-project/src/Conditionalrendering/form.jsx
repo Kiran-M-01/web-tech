ae79cb7d769f81643e418cc1f16397c2b1fe134b
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-09-12 12:06:24 +0530

    form

--- react/vite-project/src/Conditionalrendering/form.jsx
+++ react/vite-project/src/Conditionalrendering/form.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const Form = () => {
-    let [state, setState] = useState(false)
-    function change(){
-        setState(!state)
+    let [eye,setEye] = useState(false)
+    let handleClick = () => {
+        setEye(!eye) 
     }
 
     return(
@@ -14,9 +16,12 @@ const Form = () => {
                     <label htmlFor="">User Name</label>
                     <input type="text" placeholder="enter username" />
                 </div>
-                <div>
+                <div id="password_section">
                     <label htmlFor="">Password</label>
-                    <input type="password"  placeholder="enter password" />
+                    <input type={eye?"text":"password"}  placeholder="enter password" />
+                    <section id="eye_section" onClick={handleClick}>
+                        { eye? <FaEye />: <FaEyeSlash />}
+                    </section>
                 </div>
                 <div>
                     <label htmlFor="" > Email</label>
@@ -24,7 +29,7 @@ const Form = () => {
                 </div>
                 <div>
                     <label htmlFor="">confirm password</label>
-                    <input type="passowrd"  placeholde="confirm password"/>
+                    <input type="password"  placeholde=""/>
                 </div>
                 <button>Register</button>
             </form>
